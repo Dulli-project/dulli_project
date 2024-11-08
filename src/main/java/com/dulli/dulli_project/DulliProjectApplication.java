a027e2d4b86a292906463a492c7f440436d018da
Jisu <wltn214400@gmail.com>
2024-11-08 13:00:48 +0900

    init

---
+++ src/main/java/com/dulli/dulli_project/DulliProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dulli.dulli_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DulliProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DulliProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dulli/dulli_project/DulliProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulli.dulli_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DulliProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
